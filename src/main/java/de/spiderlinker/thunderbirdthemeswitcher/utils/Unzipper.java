81ee17fdf415349308b0b52050505575d9e95db9
Spiderlinker <support@spiderlinker.de>
2018-02-11 21:04:45 +0100

    Structural change

--- src/main/java/de/spiderlinker/thunderbirdthemeswitcher/utils/Unzipper.java
+++ src/main/java/de/spiderlinker/thunderbirdthemeswitcher/utils/Unzipper.java
@@ -1,65 +1,48 @@
 package de.spiderlinker.thunderbirdthemeswitcher.utils;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Unzipper {
-	/**
-	 * Size of the buffer to read/write data
-	 */
-	private static final int BUFFER_SIZE = 4096;
 
-	/**
-	 * Extracts a zip file specified by the zipFilePath to a directory specified by
-	 * destDirectory (will be created if does not exists)
-	 * 
-	 * @param zipFilePath
-	 * @param destDirectory
-	 * @throws IOException
-	 */
-	public static void unzip(String zipFilePath, String destDirectory) throws IOException {
-		File destDir = new File(destDirectory);
-		if (!destDir.exists()) {
-			destDir.mkdir();
-		}
-		ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
-		ZipEntry entry = zipIn.getNextEntry();
-		// iterates over entries in the zip file
-		while (entry != null) {
-			String filePath = destDirectory + File.separator + entry.getName();
-			if (!entry.isDirectory()) {
-				// if the entry is a file, extracts it
-				extractFile(zipIn, filePath);
-			} else {
-				// if the entry is a directory, make the directory
-				File dir = new File(filePath);
-				dir.mkdir();
-			}
-			zipIn.closeEntry();
-			entry = zipIn.getNextEntry();
-		}
-		zipIn.close();
-	}
+  private static final int BUFFER_SIZE = 4096;
 
-	/**
-	 * Extracts a zip entry (file entry)
-	 * 
-	 * @param zipIn
-	 * @param filePath
-	 * @throws IOException
-	 */
-	private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
-		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
-		byte[] bytesIn = new byte[BUFFER_SIZE];
-		int read = 0;
-		while ((read = zipIn.read(bytesIn)) != -1) {
-			bos.write(bytesIn, 0, read);
-		}
-		bos.close();
-	}
+  public static void unzip(String zipFilePath, String destDirectory) throws IOException {
+    createDirectory(destDirectory);
+
+    try (ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath))) {
+      ZipEntry entry;
+      while ((entry = zipIn.getNextEntry()) != null) {
+        extractEntryOfZipToDirectory(zipIn, entry, destDirectory);
+      }
+    }
+  }
+
+  private static void createDirectory(String directory){
+    File dir = new File(directory);
+    if(!dir.exists()){
+      dir.mkdir();
+    }
+  }
+
+  private static void extractEntryOfZipToDirectory(ZipInputStream zipInput, ZipEntry entry, String destinationDir) throws IOException {
+    String filePath = destinationDir + File.separator + entry.getName();
+    if (!entry.isDirectory()) {
+      extractFile(zipInput, filePath);
+    } else {
+      createDirectory(filePath);
+    }
+    zipInput.closeEntry();
+  }
+
+  private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+    try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath))) {
+      byte[] bytesIn = new byte[BUFFER_SIZE];
+      int read;
+      while ((read = zipIn.read(bytesIn)) != -1) {
+        bos.write(bytesIn, 0, read);
+      }
+    }
+  }
 }
