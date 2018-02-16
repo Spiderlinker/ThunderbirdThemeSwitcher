8cbb3a283e79fbe351c17f78f6817ed1358a71ec
Spiderlinker <support@spiderlinker.de>
2018-02-16 16:51:27 +0100

    Minor structural changes

--- src/main/java/de/spiderlinker/thunderbirdthemeswitcher/ThunderbirdThemeSwitcher.java
+++ src/main/java/de/spiderlinker/thunderbirdthemeswitcher/ThunderbirdThemeSwitcher.java
@@ -8,18 +8,39 @@ import javafx.stage.Stage;
 
 public class ThunderbirdThemeSwitcher extends Application {
 
+  private Stage stage;
+  private Scene scene;
+
   public static void main(String[] args) {
     launch(args);
   }
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage primaryStage) {
     System.out.println(ThunderbirdUtils.getProfileFolders());
-    Scene scene = new Scene(new MainView().getRoot());
-    scene.getStylesheets().add(getClass().getClassLoader().getResource("css/ProgressBarStyle.css").toExternalForm());
-    primaryStage.setScene(scene);
-    primaryStage.show();
+    this.stage = primaryStage;
+
+    createSceneForStage();
+    setupSceneStylesheet();
+    setupStageTitle();
+    showApplication();
   }
 
+  private void createSceneForStage() {
+    this.scene = new Scene(new MainView().getRoot());
+    stage.setScene(scene);
+  }
+
+  private void setupSceneStylesheet() {
+    scene.getStylesheets().add(getClass().getClassLoader().getResource("css/ProgressBarStyle.css").toExternalForm());
+  }
+
+  private void setupStageTitle() {
+    stage.setTitle("Thunderbird Theme Switcher - 1.0 - by Spiderlinker");
+  }
+
+  private void showApplication() {
+    stage.show();
+  }
 
 }
--- src/main/java/de/spiderlinker/thunderbirdthemeswitcher/core/ThemeUninstaller.java
+++ src/main/java/de/spiderlinker/thunderbirdthemeswitcher/core/ThemeUninstaller.java
@@ -1,31 +1,30 @@
 package de.spiderlinker.thunderbirdthemeswitcher.core;
 
-import java.io.File;
-import java.io.IOException;
-
 import de.spiderlinker.thunderbirdthemeswitcher.Config;
 import de.spiderlinker.thunderbirdthemeswitcher.utils.ThunderbirdUtils;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+
 public class ThemeUninstaller {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ThemeUninstaller.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(ThemeUninstaller.class);
 
-	public static void uninstallTheme() {
-		LOGGER.info("Uninstalling theme...");
-		deleteDirectory(new File(Config.DIR_TEMP));
-		ThunderbirdUtils.getProfileFolders()
-				.forEach(profileFolder -> deleteDirectory(new File(profileFolder, Config.THEME_FOLDER_NAME)));
-	}
+  public static void uninstallTheme() {
+    LOGGER.info("Uninstalling theme...");
+    deleteDirectory(new File(Config.DIR_TEMP));
+    ThunderbirdUtils.getProfileFolders()
+        .forEach(ThemeUninstaller::deleteThemeDirectoryOf);
+  }
 
-	private static void deleteDirectory(File directory) {
-		try {
-			FileUtils.deleteDirectory(directory);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  private static void deleteThemeDirectoryOf(File profileFolder) {
+    deleteDirectory(new File(profileFolder, Config.THEME_FOLDER_NAME));
+  }
+
+  private static void deleteDirectory(File directory) {
+    FileUtils.deleteQuietly(directory);
+  }
 
 }
